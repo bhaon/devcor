c15c92d6051e2055fb7601fd55e47e9336a396f8
bhaon <bhaon13@gmail.com>
2024-06-13 19:44:23 +0200

    Update Solid_SRP.py

--- Solid_SRP.py
+++ Solid_SRP.py
@@ -1,1 +1,13 @@
+class NetworkService:
+  def __init__(self, name, config):
+    self.name = name
+    self.config = config
 
+  def set_name(self, name):
+    self.name = name
+
+  def set_config(self, config):
+    self.config = config
+
+  def deploy_service(self):
+    Infrastructure.deploy(self.name, self.config)
